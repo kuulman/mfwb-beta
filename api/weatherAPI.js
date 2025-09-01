9d8fb4662bca98f928e16e88bfe1f9933bcb659b
kuulman <cocoiscoolman@gmail.com>
2025-09-01 21:12:57 +0700

    benerin backend weather

--- api/weatherAPI.js
+++ api/weatherAPI.js
@@ -3,27 +3,35 @@ const express = require('express');
 const app = express();
 let city = 'Karawang';
 
-async function getWeather(city) {
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+async function getWeather() {
   try {
     const response = await axios.get('https://api.ryzumi.vip/api/search/weather?city=' + city);
     const data = response.data;
-    const cityOutput = data.name
+    const cityOutput = data.name;
+    const weather = capitalize(data.weather[0].description);
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+    console.log(`City: ${cityOutput}\nWeather: ${weather}\nTemperature: ${temp}°C\nHumidity: ${humidity}%\nWind Speed: ${windSpeed} m/s`);
   } catch (error) {
-    throw new Error('Failed to fetch weather data: ' + error.message);
+    console.error('Failed to fetch weather data: ' + error.message);
   }
 }
 
 app.get('/weatherapi', async (req, res) => {
   try {
-    const response = await getWeather(city);
-    res.json(response);
+    const response = await axios.get('https://api.ryzumi.vip/api/search/weather?city=' + city);
+    res.json(response.data);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
+    res.status(500).json({ error: 'Failed to fetch data: ' + error.message });
   }
 });
 
 app.listen(3500, () => {
   console.log(`weatherAPI is running on port 3500/weatherapi`);
+  getWeather()
 });
 
 module.exports = {
